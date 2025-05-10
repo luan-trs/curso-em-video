1bbd671286c7a0004a433519975e4ec77bd953d5
luan-trs <luantrsilva@gmail.com>
2025-05-09 21:01:05 -0300

    Create 075.py
    início do exercício

---
+++ python/!exercicios_python/075.py
@@ -0,0 +1,5 @@
+num = (int(input('Digite um número: ')),
+       int(input('Digite outro número: ')),
+       int(input('Digite mais um número: ')),
+       int(input('Digite o último número: '))
+       )
