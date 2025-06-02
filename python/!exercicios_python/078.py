deddec4967da76d3c6e2b2ba09abc54f10c292ee
luan-trs <luantrsilva@gmail.com>
2025-06-02 16:34:38 -0300

    Exercício 078
    Feito

--- python/!exercicios_python/078.py
+++ python/!exercicios_python/078.py
@@ -1,8 +1,18 @@
 '''Faça um programa que leia 5 valores numéricos e guarde-os em uma lista.
 No final, mostre qual foi o maior e o menor valor digitado e as suas posições na lista'''
 nums = []
-for n in range(5):
-    nums.append(int(input(f'Digite um valor para a posição {n}: ')))
+maior = indexMaior =  -1
+menor = indexMenor = 999
 
-maior = nums[0]
-menor = nums[0]
+for n in range(5):
+    add = int(input(f"Digite um valor para a posição {n}: "))
+    nums.append(add)
+    if add > maior:
+        maior = add
+        indexMaior = nums.index(add)
+    if add < menor:
+        menor = add
+        indexMenor = nums.index(add)
+
+print(f"\nO maior número foi: {maior}, na posição {indexMaior}")
+print(f"O menor número foi {menor} na posição {indexMenor}")
