640af1c7f76d37c08fe7d7b83ead285f27c69b62
Luan <luantrsilva@gmail.com>
2025-06-02 18:26:12 -0300

    Exercício 082
    feito

--- python/!exercicios_python/082.py
+++ python/!exercicios_python/082.py
@@ -1,3 +1,24 @@
 '''Crie um programa que leia vários números e coloque-os em uma lista.
 Depois disso, crie  duas listas extras que vão conter apenas os valores pares e os valores ímpares digitados,
 respectivamente. No final mostre o conteúdo das três listas geradas.'''
+listaTotal = []
+listaPar = []
+listaImpar = []
+
+while True:
+    num = int(input("Insira um valor: "))
+    listaTotal.append(num)
+    
+    user = input("Deseja continuar? [s/n] ").lower()
+    if user == 'n':
+        break
+
+for n in listaTotal:
+    if n%2 == 0:
+        listaPar.append(n)
+    else:
+        listaImpar.append(n)
+        
+print(f"\nNúmeros digitados: {listaTotal}")
+print(f"Números pares: {listaPar}")
+print(f"Números ímpares: {listaImpar}")
