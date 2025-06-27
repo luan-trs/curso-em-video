e4b78b931dadb948761a0e9e70a6b6a57c42d396
luan-trs <luantrsilva@gmail.com>
2025-06-27 19:17:53 -0300

    Exercicio 83

--- python/!exercicios_python/083.py
+++ python/!exercicios_python/083.py
@@ -1,3 +1,23 @@
 '''Crie um programa onde o usuário digite uma expressão qualquer que use parênteses.
 Seu aplicativo deverá analisar se a expressão passada está com os parênteses abertos e fechados
-na ordem correta'''
+na ordem correta.'''
+parenteses = [] #Lista onde avaliarei a posição dos parenteses
+parentesesAbertos = parentesesFechados = 0
+erros = 0
+
+expressao = input("Digite uma expressão numérica: ")
+for c in range(len(expressao)):
+    if expressao[c] in "(":
+        parenteses.append(expressao[c])
+        parentesesAbertos+=1
+    elif expressao[c] in ")":
+        parenteses.append(expressao[c])
+        parentesesFechados+=1
+        if parentesesAbertos > parentesesFechados or parentesesAbertos < parentesesFechados:
+            erros += 1
+    
+
+if erros < 1:
+    print("\033[32mNenhum erro encontrado")
+else:
+    print(f"\033[31m{erros} parentêses estão escritos na ordem incorreta")
