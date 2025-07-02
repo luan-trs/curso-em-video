7cf70779e96818daf50d021c22c902e9789998ce
luan-trs <luantrsilva@gmail.com>
2025-07-02 18:03:16 -0300

    Create 089.py

---
+++ python/!exercicios_python/089.py
@@ -0,0 +1,3 @@
+'''Ler o nome e duas notas de vários alunos e guardar em uma lista composta.
+No final, mostre um boletim contendo a média de cada um e permita com que o usuário possa mostrar as notas
+de cada um individualmente (999 interrompe)'''
