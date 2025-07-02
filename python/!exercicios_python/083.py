b07ed54e2416b25afd553594e16f4aef8b2dfbad
luan-trs <luantrsilva@gmail.com>
2025-07-02 13:03:26 -0300

    Exercicio 83 corrigido

--- python/!exercicios_python/083.py
+++ python/!exercicios_python/083.py
@@ -1,23 +1,24 @@
 '''Crie um programa onde o usuário digite uma expressão qualquer que use parênteses.
 Seu aplicativo deverá analisar se a expressão passada está com os parênteses abertos e fechados
 na ordem correta.'''
-parenteses = [] #Lista onde avaliarei a posição dos parenteses
-parentesesAbertos = parentesesFechados = 0
-erros = 0
 
 expressao = input("Digite uma expressão numérica: ")
-for c in range(len(expressao)):
-    if expressao[c] in "(":
-        parenteses.append(expressao[c])
-        parentesesAbertos+=1
-    elif expressao[c] in ")":
-        parenteses.append(expressao[c])
-        parentesesFechados+=1
-        if parentesesAbertos > parentesesFechados or parentesesAbertos < parentesesFechados:
-            erros += 1
+pilha = [] #Armazena os parenteses errados
+
+for c in expressao:
+    #Caso o caractere seja um parenteses aberto, ele é adicionado a pilha
+    if c == '(':
+        pilha.append(c)
+    #Caso o parenteses feche, o par é removido da pilha e nao representam um erro
+    elif c == ')':
+        if len(pilha) > 0:
+            pilha.pop()
+        else:
+            pilha.append(c) #Erro identificado
+            break
     
 
-if erros < 1:
-    print("\033[32mNenhum erro encontrado")
+if len(pilha) == 0:
+    print("\033[32mNenhum erro encontrado.\033[0m")
 else:
-    print(f"\033[31m{erros} parentêses estão escritos na ordem incorreta")
+    print(f"\033[31mSua expressão está errada!\033[0m")
