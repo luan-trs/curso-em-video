ce4e733ae9b417223df1de2b452d69ea26e0ab3b
luan-trs <luantrsilva@gmail.com>
2025-07-02 11:46:58 -0300

    Exercicio 78 corrigido

--- python/!exercicios_python/078.py
+++ python/!exercicios_python/078.py
@@ -1,18 +1,30 @@
 '''Faça um programa que leia 5 valores numéricos e guarde-os em uma lista.
 No final, mostre qual foi o maior e o menor valor digitado e as suas posições na lista'''
 nums = []
-maior = indexMaior =  -1
-menor = indexMenor = 999
+maior = 0
+menor = 0
 
 for n in range(5):
-    add = int(input(f"Digite um valor para a posição {n}: "))
-    nums.append(add)
-    if add > maior:
-        maior = add
-        indexMaior = nums.index(add)
-    if add < menor:
-        menor = add
-        indexMenor = nums.index(add)
+    nums.append(int(input(f"Digite um valor para a posição {n}: ")))
+    if n == 0:
+        maior = menor = nums[n]
+    else:
+        if nums[n] > maior:
+            maior = nums[n]
+        if nums[n] < menor:
+            menor = nums[n]
+print('-=-'*15)
+print(f'Você digitou os valores {nums}')
 
-print(f"\nO maior número foi: {maior}, na posição {indexMaior}")
-print(f"O menor número foi {menor} na posição {indexMenor}")
+#Verifica as posições do maior e menor número e mostra todos os index
+print(f"\nO maior valor foi: {maior}, nas posições ", end='')
+for i, v in enumerate(nums):
+    if v == maior:
+        print(f'{i}... ', end='')
+print()
+
+print(f"\nO menor valor foi: {menor}, nas posições ", end='')
+for i, v in enumerate(nums):
+    if v == menor:
+        print(f'{i}... ', end='')
+print()
