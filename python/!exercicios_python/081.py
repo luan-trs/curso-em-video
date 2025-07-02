c93d7a0b5d1941fecdcb6e5f3b97c134511dd28a
luan-trs <luantrsilva@gmail.com>
2025-07-02 12:31:02 -0300

    Exercicio 81 corrigido

--- python/!exercicios_python/081.py
+++ python/!exercicios_python/081.py
@@ -4,12 +4,10 @@ A) Quantos números foram digitados
 B) A lista de valores ordenados de forma decrescente
 C) Se o valor 5 foi digitado e está ou não na lista'''
 lista = []
-quant = 0
 
 while True:
     num = int(input("Insira um valor: "))
     lista.append(num)
-    quant+=1
     
     user = input("Deseja continuar? [s/n] ").lower()
     if user == 'n':
@@ -17,7 +15,7 @@ while True:
 
 lista.sort(reverse=True)
 
-print(f"\nA) A quantidade de valores digitados foi de: {quant} valores")
+print(f"\nA) A quantidade de valores digitados foi de: {len(lista)} valores")
 print(f"B) Valores ordenados de forma decrescente: {lista}")
 if 5 in lista:
     print("C) O número 5 foi digitado e está na lista.")
