7fb28440526abd9d917f2b9ed80ac16723f6ad32
luan-trs <luantrsilva@gmail.com>
2025-07-04 13:02:05 -0300

    Exercicio 88 corrigido
    Já estava pronto, porém em alguns casos o número poderia se repetir

--- python/!exercicios_python/088.py
+++ python/!exercicios_python/088.py
@@ -8,15 +8,13 @@ jogos = int(input('Quantos jogos deseja gerar? '))
 
 print('\033[33m')
 for i in range(jogos):
-    for j in range(6):
+    cont = 0
+    while cont < 6: #Garante que 6 números serão gerados
         num = randint(1, 60)
-        if num not in jogo:
+        if num not in jogo: #Garante que todos serão diferentes
             jogo.append(num)
-        else:
-            num = randint(1, 60)
-            jogo.append(num)
+            cont += 1 
     resultados.append(jogo[:])
     jogo.clear()
-
     print(f'JOGO {i+1}: {resultados[i]}')
-print('\033[34m-=-=-=-= BOA SORTE =-=-=-=-=-\033[0m')
+print('\033[34m-=-=-=-=- BOA SORTE =-=-=-=-=-\033[0m')
