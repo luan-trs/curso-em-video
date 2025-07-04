4b548ab2685d8d03515fbffdef73650dfaf62b35
luan-trs <luantrsilva@gmail.com>
2025-07-04 12:12:20 -0300

    Exercicio 084 corrigido
    A minha solução foi parecida, porém faltou a listagem de pessoas

--- python/!exercicios_python/084.py
+++ python/!exercicios_python/084.py
@@ -6,28 +6,29 @@ pessoas = []
 dados = []
 
 while True:
-    dados.append(input('Nome: '))
+    dados.append(str(input('Nome: ')))
     dados.append(float(input('Peso: ')))
+    if len(pessoas) == 0:
+        maior = menor = dados[1]
+    else:
+        if dados[1] > maior:
+            maior = dados[1]
+        if dados[1] < menor:
+            menor = dados[1]
     pessoas.append(dados[:]) #Guarda o array atual no array de pessoas, depois o limpa
     dados.clear()
 
     continuar = str(input('Deseja continuar? [s/n] ')).lower()
     if continuar == 'n':
         break
+print('-=-'*15)
 print(f'A) Total de pessoas cadastradas: {len(pessoas)}')
 
-#Procura o maior e o menor peso e seus nomes na matriz
-maior_peso = pessoas[0][1]
-menor_peso = pessoas[0][1]
-maior_nome = pessoas[0][0]
-menor_nome = pessoas[0][0]
-for p in range(len(pessoas)):
-    if pessoas[p][1] > maior_peso:
-        maior_peso = pessoas[p][1]
-        maior_nome = pessoas[p][0]
-    if pessoas[p][1] < menor_peso:
-        menor_peso = pessoas[p][1]
-        menor_nome = pessoas[p][0]
-print(f'B) O maior peso registrado foi: {maior_peso:.2f}kg, por {maior_nome}')
-
-print(f'C) O menor peso registrado foi: {menor_peso:.2f}kg, por {menor_nome}')
+print(f'B) O maior peso registrado foi: {maior:.2f}kg, peso de: ', end='')
+for p in pessoas:
+    if p[1] == maior:
+        print(f'[{p[0]}]', end='')
+print(f'C) O menor peso registrado foi: {menor:.2f}kg, peso de: ', end='')
+for p in pessoas:
+    if p[1] == menor:
+        print(f'[{p[0]}]', end='')
