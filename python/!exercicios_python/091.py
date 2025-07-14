edfb1fdb58e35a9d5799cade60b457fed362b885
luan-trs <luantrsilva@gmail.com>
2025-07-14 11:07:49 -0300

    Exercício 091 completo

--- python/!exercicios_python/091.py
+++ python/!exercicios_python/091.py
@@ -15,26 +15,29 @@ for i in range(4):
 
 print('\033[0m', end='')
 print('-=-'*15)
-print(f'\033[34m == RANKING DOS JOGADORES ==')
+print(f'\033[36m === RANKING DOS JOGADORES ===')
 
 #Transforma o dicionário em uma lista de tuplas
-result_desordenado = resultados.items() 
+resultados = list(resultados.items())
 
-#Guarda essas tuplas de forma ordenada utilizando o algoritmo de sorting do exercicio 80 (adaptado)
+#Guarda essas tuplas de forma decrescente utilizando o algoritmo de sorting do exercicio 80 (adaptado)
 result_ordenado = []
 for i in range(4):
-    if i == 0 or result_desordenado[i][1] > result_ordenado[-1][1]:
-        result_desordenado[i].append(result_ordenado)
+    if i == 0 or resultados[i][1] < result_ordenado[-1][1]:
+        result_ordenado.append(resultados[i])
     else:
         posicao = 0
         while posicao < len(result_ordenado):
-            if result_desordenado[i][1] <= result_ordenado[posicao][1]:
-                result_ordenado.insert(posicao, result_desordenado[i])
+            if resultados[i][1] >= result_ordenado[posicao][1]:
+                result_ordenado.insert(posicao, resultados[i])
                 break
             posicao += 1
 
 #Transforma a lista de tuplas em um dicionario
 ranking = dict(result_ordenado)
+
+#Mostra os resultados do maior ao menor
+pos = 1
 for k, v in ranking.items():
-    for i in range(4):
-        print(f' {i+1}º lugar: {k} com {v}.')
+    print(f'    {pos}º lugar: {k} com {v}.')
+    pos += 1
