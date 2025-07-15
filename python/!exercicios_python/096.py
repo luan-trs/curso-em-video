ca931900dbe08b35404bb9817397f33755f928b6
luan-trs <luantrsilva@gmail.com>
2025-07-15 20:01:11 -0300

    Exercício 096: resolução

--- python/!exercicios_python/096.py
+++ python/!exercicios_python/096.py
@@ -1,1 +1,9 @@
 '''Uma função chamada area() que recebe as dimensões de um terreno retangular e mostre a area do terreno'''
+print('  Cálculo de área  ')
+print('--'*15)
+
+def area(comprimento, largura):
+    result = comprimento*largura
+    print(f'A área de um terreno {comprimento}x{largura} é de {result:.2f}m²')
+
+area(comprimento=float(input('Comprimento (m): ')), largura=float(input('Largura (m): ')))
