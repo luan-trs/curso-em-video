02ee6e341e4703f307efd487a965feae7473c112
luan-trs <luantrsilva@gmail.com>
2025-07-16 17:53:55 -0300

    Exercício 097: resolução

--- python/!exercicios_python/097.py
+++ python/!exercicios_python/097.py
@@ -1,2 +1,9 @@
 '''Função chamada escreva() que recebe um texto como parâmetro e mostra uma mensagem com tamanho adaptável
 (As linhas de cima e debaixo devem ter o mesmo tamanho da mensagem)'''
+def escreva(texto):
+    tamanho = len(texto)+4
+    print('~'*tamanho)
+    print(f'  {texto}')
+    print('~'*tamanho)
+
+escreva('Título')
