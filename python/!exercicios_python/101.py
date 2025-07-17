22466dd86d21ed50f00e93f90e305f1ac07f1be0
luan-trs <luantrsilva@gmail.com>
2025-07-17 13:08:51 -0300

    Aula 21

---
+++ python/!exercicios_python/101.py
@@ -0,0 +1,2 @@
+'''Um programa com uma função voto() que recebe o ano de nascimento de uma pessoa, retornando um valor
+literal indicando se a pessoa tem voto NEGADO, OPCIONAL ou OBRIGATÓRIO nas eleições'''
