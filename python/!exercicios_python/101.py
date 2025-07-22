2255fbc95206518da33f85a575c09026bfb3e8bd
luan-trs <luantrsilva@gmail.com>
2025-07-22 13:04:42 -0300

    Exercício 101: resolução

--- python/!exercicios_python/101.py
+++ python/!exercicios_python/101.py
@@ -1,2 +1,16 @@
 '''Um programa com uma função voto() que recebe o ano de nascimento de uma pessoa, retornando um valor
 literal indicando se a pessoa tem voto NEGADO, OPCIONAL ou OBRIGATÓRIO nas eleições'''
+from datetime import date
+
+def voto(nascimento):
+    print('--'*20)
+    idade = date.today().year - nascimento
+    if idade < 16:
+        return f'Com {idade} anos: VOTO NEGADO.'
+    elif idade < 18 or idade >= 70:
+        return f'Com {idade} anos: VOTO OPCIONAL.'
+    else:
+        return f'Com {idade} anos: VOTO OBRIGATÓRIO.'
+
+nascimento = int(input('Em que ano você nasceu? '))
+print(voto(nascimento))
