d185c993157e7c1368fd730a79a3765aac8072a9
luan-trs <luantrsilva@gmail.com>
2025-07-22 13:42:45 -0300

    Exercício 104: resolução

--- python/!exercicios_python/104.py
+++ python/!exercicios_python/104.py
@@ -1,1 +1,12 @@
 '''Função leiaInt() que funciona de forma semelhante ao input() só que aceitando apenas valores numéricos'''
+def leiaInt(mensagem):
+    while True:
+        num = str(input(mensagem))
+        if num.isnumeric():
+            num = int(num)
+            return num
+        else:
+            print('\033[31mERRO! Digite um número inteiro válido.\033[0m')
+
+n = leiaInt('Digite um número: ')
+print(f'Você digitou o número {n}.')
