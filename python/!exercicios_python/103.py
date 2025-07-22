8574683de501f801e12a86387ca7077673854801
luan-trs <luantrsilva@gmail.com>
2025-07-22 13:42:32 -0300

    Exercício 103: resolução

--- python/!exercicios_python/103.py
+++ python/!exercicios_python/103.py
@@ -1,2 +1,14 @@
 '''Função chamada ficha() que recebe 2 parâmetros opcionais, sendo eles o nome de um jogador e quantos gols ele 
 marcou. O programa deverá mostrar a ficha do jogador mesmo que algum dado não tenha sido informado corretamente'''
+def ficha(nome="<deconhecido>", gols=0):
+    if nome.strip() == '':
+        nome = '<desconhecido>'
+    if gols.isnumeric():
+        gols = int(gols)
+    else:
+        gols = 0
+    print(f'O jogador {nome} fez {gols} gols no campeonato.')
+
+nome = str(input('Nome: '))
+gols = str(input('Gols: '))
+ficha(nome, gols)
