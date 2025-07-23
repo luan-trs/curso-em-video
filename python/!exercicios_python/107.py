0942577036025d488a54d4844ecfa068c9d46547
luan-trs <luantrsilva@gmail.com>
2025-07-23 17:05:16 -0300

    Exerícios Aula 22

---
+++ python/!exercicios_python/107.py
@@ -0,0 +1,2 @@
+'''Crie um módulo chamado moeda.py que tenhas as funções incorporadas: aumentar(), diminuir(), dobro() e metade().
+Faça também um programa que importe esse módulo e utilize essas funções'''
