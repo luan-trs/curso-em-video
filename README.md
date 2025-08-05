a8b6ce4f697ef90c5594bf4ce31f6b9cc9eeb417
luan-trs <luantrsilva@gmail.com>
2025-08-05 18:27:44 -0300

    Consertando typos e erros de caminho

--- README.md
+++
@@ -1,3 +0,0 @@
-# curso em video
-Olá!!
-Esse é o meu primeiro repositório. Fiz ele com o intuito de guardar os exercícios feitos nos cursos do professor Gustavo Guanabara.
--- python/!exercicios_python/ex112/utilidadescev/dado/__init__.py
+++ python/!exercicios_python/ex112/utilidadescev/dado/__init__.py
@@ -2,6 +2,6 @@ def leiaDinheiro(msg):
     while True:
         p = str(input(msg)).replace(',', '.').strip()
         if p.isalpha() or p == '':
-            print(f'\033[31mERRO: \"{p}\" é um preço inválido.')
+            print(f'\033[31mERRO: \"{p}\" é um preço inválido.\033[0m')
         else:
             return float(p)
